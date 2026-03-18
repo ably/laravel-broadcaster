7b41b39569c55cc206cdbaeede111df7dddde9c5
sacOO7 <sachinshinde7676@gmail.com>
2026-03-18 23:40:43 +0530

    Updated LIB_VERSION to 1.0.8 from 1.0.7, updated CHANGELOG accordingly

--- src/AblyBroadcaster.php
+++ src/AblyBroadcaster.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class AblyBroadcaster extends Broadcaster
 {
-    const LIB_VERSION = '1.0.7';
+    const LIB_VERSION = '1.0.8';
 
     /**
      * The AblyRest SDK instance.
